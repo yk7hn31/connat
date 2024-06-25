071321cb111b8ee63910d830d02350d04773e9a4
yuzuki <yk7hn31@gmail.com>
2024-06-25 13:04:39 +0000

    Add CSS

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import compression from 'compression';
 import dotenv from 'dotenv';
+import apiRouter from './router/api.js';
 
 dotenv.config();
 const app = express();
+
+app.use(compression());
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello world');
